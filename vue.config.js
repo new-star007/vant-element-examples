0ec740d3281ec33cc5e9e989f5f7fe9228d32f4d
zouhuaqiang <zouhuaqiang@tienon.com>
2026-07-16 20:24:36 +0800

    fix

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
-module.exports = {
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
   lintOnSave: false,
-  transpileDependencies: ['vant-element']
-}
+  transpileDependencies: ['vant-element'],
+  publicPath: process.env.NODE_ENV === 'production' ? '/vant-element-examples/' : '/'
+})
