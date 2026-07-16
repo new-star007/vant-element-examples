3bbc3af917833c43f4609ee5ebd55ca739246437
zouhuaqiang <zouhuaqiang@tienon.com>
2026-07-16 20:08:01 +0800

    fix

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,4 @@
-const path = require('path')
-
 module.exports = {
   lintOnSave: false,
-  transpileDependencies: ['vant-element'],
-  configureWebpack: {
-    resolve: {
-      alias: {
-        'vant-element': path.resolve(__dirname, '../vant-element/packages/vant-element/src')
-      }
-    }
-  }
+  transpileDependencies: ['vant-element']
 }
