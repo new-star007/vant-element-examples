4b511c449752a148c9d1ed9e22bdd46963b25077
zouhuaqiang <zouhuaqiang@tienon.com>
2026-07-17 09:53:46 +0800

    版本更新

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vant from 'vant'
 import 'vant/lib/index.css'
 import 'vant-element/dist/vant-element.css'
 import VantElement from 'vant-element'
 import router from './router'
 
+Vue.use(Vant)
 Vue.use(VantElement)
 
 new Vue({
